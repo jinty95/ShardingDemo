2b4a4fa7acb6fb515d190b969a9d8a5ca2275ae4
wangjintai <wangjintai@aspirecn.com>
2021-06-23 16:12:22 +0800

    删除数据表tbl_encrypt及其相关代码

--- src/main/java/cn/sharding/demo/controller/EncryptController.java
+++ src/main/java/cn/sharding/demo/controller/EncryptController.java
@@ -1,16 +1,12 @@
 package cn.sharding.demo.controller;
 
-import cn.sharding.demo.entity.Encrypt;
-import cn.sharding.demo.service.EncryptService;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.shardingsphere.encrypt.algorithm.AESEncryptAlgorithm;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Base64;
-import java.util.List;
 
 /**
  * Controller
@@ -24,9 +20,6 @@ import java.util.List;
 public class EncryptController {
 
     @Autowired
-    private EncryptService encryptService;
-
-    @Autowired
     private AESEncryptAlgorithm aesEncryptAlgorithm;
 
     @RequestMapping("/getAesKey")
@@ -35,59 +28,6 @@ public class EncryptController {
         String base64Key = Base64.getEncoder().encodeToString(key);
         log.info("Real Aes Key is '{}' [Base64 Format]",base64Key);
         return base64Key;
-    }
-
-    @RequestMapping("/insert")
-    public String insert(@RequestBody Encrypt data){
-        try{
-            long begin = System.currentTimeMillis();
-            encryptService.insert(data);
-            long end = System.currentTimeMillis();
-            log.info("保存记录耗时：used={}ms",end-begin);
-        }catch (Exception e){
-            log.error("保存记录异常！data="+data,e);
-            return "保存失败";
-        }
-        return "保存成功";
-    }
-
-    @RequestMapping("/insertBatch")
-    public String insertBatch(@RequestBody List<Encrypt> data){
-        try{
-            long begin = System.currentTimeMillis();
-            encryptService.insertBatch(data);
-            long end = System.currentTimeMillis();
-            log.info("保存记录耗时：used={}ms",end-begin);
-        }catch (Exception e){
-            log.error("保存记录异常！data="+data,e);
-            return "保存失败";
-        }
-        return "保存成功";
-    }
-
-    @RequestMapping("getById")
-    public Encrypt getById(Integer id){
-        try{
-            Encrypt data = encryptService.getById(id);
-            return data;
-        }catch (Exception e){
-            log.error("查询记录异常！id="+id,e);
-            return null;
-        }
-    }
-
-    @RequestMapping("list")
-    public List<Encrypt> list(Encrypt data){
-        try{
-            long begin = System.currentTimeMillis();
-            List<Encrypt> list = encryptService.list(data);
-            long end = System.currentTimeMillis();
-            log.info("查询记录耗时：used={}",end-begin);
-            return list;
-        }catch (Exception e){
-            log.error("查询记录异常！data="+data,e);
-            return null;
-        }
     }
 
 }
